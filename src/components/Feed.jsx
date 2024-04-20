66a744ef9285902b6bd7abf3507c42b2764373ac
hr991117 <harshkumarrayan108@gmail.com>
2024-04-20 20:22:30 +0530

    first commit

---
+++ src/components/Feed.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect, useState } from "react";
+import { Box, Stack, Typography } from "@mui/material";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
+import { Videos, Sidebar } from "./";
+
+const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [selectedCategory]);
+
+  return (
+    <Stack
+      sx={{
+        flexDirection: { sx: "column", md: "row" },
+      }}
+    >
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
+        <Sidebar
+          selectedCategory={selectedCategory}
+          setSelectedCategory={setSelectedCategory}
+        />
+
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
+          Copyright © 2022 Harsh Media
+        </Typography>
+      </Box>
+
+      <Box p={2} sx={{ overflowY: "auto", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          <span style={{ color: "white" }}>{selectedCategory}</span>
+          <span style={{ color: "#38c2f8" }}>Videos</span>
+        </Typography>
+        <div className="vdo">
+          <Videos videos={videos} />
+        </div>
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
---
+++ src/utils/constants.js
@@ -0,0 +1,48 @@
+import { GrReactjs } from "react-icons/gr";
+import { TbBrandNextjs } from "react-icons/tb";
+import { LuDollarSign } from "react-icons/lu";
+import {
+  MdOutlineSportsCricket,
+  MdSchool,
+  MdLiveTv,
+  MdCheckroom,
+  MdFaceRetouchingNatural,
+  MdTheaterComedy,
+} from "react-icons/md";
+import { GoHome } from "react-icons/go";
+import { BsCodeSlash, BsMusicNote } from "react-icons/bs";
+import { SiGooglepodcasts } from "react-icons/si";
+import { BiMoviePlay } from "react-icons/bi";
+import { CgGym } from "react-icons/cg";
+import { FaGamepad } from "react-icons/fa";
+
+export const logo = "https://cdn-icons-png.flaticon.com/512/2164/2164972.png";
+
+export const categories = [
+  { name: "New", icon: <GoHome /> },
+  { name: "College Wallah", icon: <BsCodeSlash /> },
+  { name: "Coding", icon: <BsCodeSlash /> },
+  { name: "ReactJS", icon: <GrReactjs /> },
+  { name: "NextJS", icon: <TbBrandNextjs /> },
+  { name: "Music", icon: <BsMusicNote /> },
+  { name: "Education", icon: <MdSchool /> },
+  { name: "Podcast", icon: <SiGooglepodcasts /> },
+  { name: "Movie", icon: <BiMoviePlay /> },
+  { name: "Gaming", icon: <FaGamepad /> },
+  { name: "Live", icon: <MdLiveTv /> },
+  { name: "Sport", icon: <MdOutlineSportsCricket /> },
+  { name: "Fashion", icon: <MdCheckroom /> },
+  { name: "Beauty", icon: <MdFaceRetouchingNatural /> },
+  { name: "Comedy", icon: <MdTheaterComedy /> },
+  { name: "Gym", icon: <CgGym /> },
+  { name: "Crypto", icon: <LuDollarSign /> },
+];
+
+export const demoThumbnailUrl = "https://i.ibb.co/G2L2Gwp/API-Course.png";
+export const demoChannelUrl = "/channel/UCDrf0V4fcBr5FlCtKwvpfwA";
+export const demoVideoUrl = "/video/bL-o2xBENY0";
+export const demoChannelTitle = "College Wallah";
+export const demoVideoTitle =
+  "Introduction to C++ | Lecture 1 | C++ and DSA Foundation Course";
+export const demoProfilePicture =
+  "http://dergipark.org.tr/assets/app/images/buddy_sample.png";
